91125929fa2f397628fe402068b1fba30cabcfce
jason <jason@jasonui-MacBook.local>
2018-05-14 00:18:46 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import App from '@/App';
+import store from '@/store/store';
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  components: { App },
+  template: '<App/>'
+})
